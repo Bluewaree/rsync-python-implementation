7913b55daad2094fbf6315d083b51b9a92b76400
bluewaree <belkihelmehdi2@gmail.com>
2020-12-24 20:55:52 +0100

    feat: add file update and creation syncing between client and server

--- client.py
+++ client.py
@@ -4,21 +4,55 @@ from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
 import os
 from pathlib import Path
+from client.helpers import adler32_chunk, md5_chunk, _get_block_list
+from client.constants import BLOCK_SIZE, EVENT_LIMIT
+from datetime import datetime
+import socket
+import json
+import threading
+from _thread import *
+import pickle
+import re
 
+def handle_update_or_create(event_path, src_path):
+    data = json.dumps({"action": "file_updated", "path": event_path}).encode("utf-8")
+    s = initiate_socket()
+    s.send(data)
+    msg = s.recv(BLOCK_SIZE)
+    checksums = msg
+    while msg:
+        try:
+            s.settimeout(2.0)
+            msg = s.recv(BLOCK_SIZE)
+        except socket.timeout:
+            break
+        s.settimeout(None)
+        checksums += msg
+    checksums = json.loads(checksums.decode("utf-8"))
+    blocks = pickle.dumps(_get_block_list(src_path, checksums))
+    s.send(blocks)
 
 class FileEventHandler(PatternMatchingEventHandler):
     def __init__(self, monitored_folder, *args, **kwargs):
         super(FileEventHandler, self).__init__(*args, **kwargs)
         self.monitored_folder = f"{str(monitored_folder)}/"
+        self.rate_limit = {}
 
     def on_any_event(self, event):
-        event_path = event.src_path.replace(self.monitored_folder, '')
-        if event.event_type=="created" and not event.is_directory: # On file create, deletion, or update
-            try: # On file creation or update
-                open(event.src_path, 'r').close()
-                pass
-            except OSError: # On file deletion
-                pass
+        striped_src_path = re.sub(r'^/private', '', event.src_path)
+        event_path = striped_src_path.replace(self.monitored_folder, '')
+        if (event.event_type=="created" or event.event_type=="modified") and not event.is_directory: # On file update
+            curr = datetime.now()
+            allow = True
+            if event_path in self.rate_limit and (curr - self.rate_limit[event_path]).seconds<=EVENT_LIMIT:
+                allow = False
+            self.rate_limit[event_path] = curr
+            if allow:
+                try: # On file creation or update
+                    open(striped_src_path, 'r').close()
+                    start_new_thread(handle_update_or_create, (event_path, striped_src_path))
+                except OSError: # On file deletion
+                    pass
         elif event.event_type=="created" and event.is_directory: # On folder creation
             if len(os.listdir(event.src_path)) == 0:
                 pass
@@ -26,8 +60,14 @@ class FileEventHandler(PatternMatchingEventHandler):
             pass
 
 
-if __name__ == "__main__":
-    path = sys.argv[1] if len(sys.argv) > 1 else '.'
+def initiate_socket():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    HOST, PORT = "localhost", 9999
+    s.connect((HOST, PORT))
+    return s
+
+
+def initiate_filehandler(path):
     absolute_path = Path(path).absolute()
     patterns = "*"
     ignore_patterns = ["*~"]
@@ -37,6 +77,12 @@ if __name__ == "__main__":
     observer = Observer()
     observer.schedule(event_handler, path, recursive=False)
     observer.start()
+
+
+if __name__ == "__main__":
+    path = sys.argv[1] if len(sys.argv) > 1 else '.'
+    initiate_filehandler(path)
+
     try:
         while True:
             time.sleep(1)
--- server.py
+++ server.py
@@ -2,10 +2,63 @@ import socket
 from _thread import *
 import threading
 import sys
+from server.helpers import adler32_chunk, md5_chunk, checksums_file
+from server.constants import BLOCK_SIZE
+import json
+import os
+import time
+from pathlib import Path
+import pickle
 
 
-def client_handler(c, path):
-    pass
+def write_blocks_to_file(blocks, absolute_file_path, tmp_file):
+    if os.path.exists(absolute_file_path):
+        with open(absolute_file_path, "rb") as ft:
+            with open(tmp_file, "wb") as f:
+                for block in blocks:
+                    if isinstance(block, int):
+                        ft.seek(block * BLOCK_SIZE)
+                        content = ft.read(BLOCK_SIZE)
+                        f.write(content)
+                    else:
+                        f.write(block)
+
+                f.close()
+                os.remove(absolute_file_path)
+                os.rename(tmp_file, absolute_file_path)
+            ft.close()
+    else:
+        with open(absolute_file_path, "wb") as f:
+            for block in blocks:
+                f.write(block)
+
+
+def handle_file_update(c, absolute_file_path, absolute_folder_path, file_path):
+    checksums = checksums_file(absolute_file_path)
+    data = json.dumps(checksums).encode("utf-8")
+    c.send(data)
+    blocks = []
+    while True:
+        data = c.recv(BLOCK_SIZE)
+        if not data:
+            break
+        blocks.append(data)
+    blocks= pickle.loads(b"".join(blocks))
+    tmp_file = f"{absolute_folder_path}/tmp_{os.path.splitext(file_path)[0]}{os.path.splitext(file_path)[1]}"
+    write_blocks_to_file(blocks, absolute_file_path, tmp_file)
+
+
+def client_handler(c, folder_path):
+    absolute_folder_path = Path(folder_path).absolute()
+    data = c.recv(BLOCK_SIZE)
+    data = json.loads(data.decode())
+    file_path = data["path"]
+    action = data["action"]
+    absolute_file_path = f"{absolute_folder_path}/{file_path}"
+    if action == "file_updated":
+        handle_file_update(c, absolute_file_path, absolute_folder_path, file_path)
+    c.close()
+
 
 if __name__ == '__main__':
     path = sys.argv[1] if len(sys.argv) > 1 else '.'
